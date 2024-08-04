96dba6ab2e79a1614ddbd53783d814c88daafe9b
barrettgp <102996326+barrettgp@users.noreply.github.com>
2024-08-04 13:19:12 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,30 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .add_systems(Update, tick)
+        .insert_resource(Custom::new())
         .run();
 }
 
 
 
-fn setup() {
+fn setup(
+    mut commands: Commands,
+) {
 
 }
 
 
 fn tick() {
 
+}
+
+
+#[derive(Resource)]
+struct Custom(f32);
+
+
+impl Custom {
+    fn new() -> Self {
+        return Custom(1.)
+    }
 }
